6a47a078d2bebf8d2e06f8a637ba341f018c16e6
damiro <hello.damiro@gmail.com>
2024-01-01 01:48:21 +0800

    resolving not found page issue

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,32 @@
-import { allPages } from 'contentlayer/generated';
-import { notFound } from 'next/navigation';
-import Main from '@/components/Main';
+'use client';
+
+import { useEffect } from 'react';
+import { useColor } from '@/providers/ColorProvider';
+import { usePath } from '@/providers/PathProvider';
+import SectionWriteup from '@/components/SectionWriteup';
+import MdxButton from '@/components/MdxButton';
 
 export default function NotFound() {
-  const mdFile = 'not-found';
-  const page = allPages.find((page) => page.slugAsParams === mdFile);
-  if (!page) notFound();
+  const { setCover } = usePath();
+  const { color, setColor } = useColor();
 
-  return <Main page={page} />;
+  useEffect(() => {
+    setColor && setColor('#71717a'); // Default primary color zinc-500
+
+    const timer = setTimeout(() => setCover(false), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <main className='w-full min-h-screen flex flex-col lg:flex-row'>
+      <SectionWriteup
+        title={'error 404'}
+        preText={'NOT FOUND'}
+        description={"What you're looking for does not exist"}
+        color={color}
+      >
+        <MdxButton href='/'>RETURN HOME</MdxButton>
+      </SectionWriteup>
+    </main>
+  );
 }

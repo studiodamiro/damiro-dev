1cbe16ea9cba0c35f43d0eed5de63850354739cd
damiro <hello.damiro@gmail.com>
2024-01-01 01:54:40 +0800

    resolving not found page issue

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,32 +1,12 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useColor } from '@/providers/ColorProvider';
-import { usePath } from '@/providers/PathProvider';
-import SectionWriteup from '@/components/SectionWriteup';
-import MdxButton from '@/components/MdxButton';
+import { useRouter } from 'next/navigation';
 
 export default function NotFound() {
-  const { setCover } = usePath();
-  const { color, setColor } = useColor();
+  const route = useRouter();
 
   useEffect(() => {
-    setColor && setColor('#71717a'); // Default primary color zinc-500
-
-    const timer = setTimeout(() => setCover(false), 500);
-    return () => clearTimeout(timer);
+    route.push('/not-found');
   }, []);
-
-  return (
-    <main className='w-full min-h-screen flex flex-col lg:flex-row'>
-      <SectionWriteup
-        title={'error 404'}
-        preText={'NOT FOUND'}
-        description={"What you're looking for does not exist"}
-        color={color}
-      >
-        <MdxButton href='/'>RETURN HOME</MdxButton>
-      </SectionWriteup>
-    </main>
-  );
 }
